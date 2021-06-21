c5aa113e0eb31501a7613f006db63b2a02b16dc7
kadircekim07 <33603691+kadircekim07@users.noreply.github.com>
2021-06-21 16:19:11 +0300

    profile page responsive bug fix

--- components/ProfilePage/profilecard.vue
+++ components/ProfilePage/profilecard.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="bg-white border flex p-6 mt-66 w-500 h-full lg:mx-profilecard sm:mx-smprofilecard ">
-<div id="header" class="flex items-center mb-4">
+  <div
+    class="
+      bg-white
+      border
+      flex
+      p-6
+      mt-66
+      sm:mx-smprofilecard
+      grid grid-cols-1
+      md:grid-cols-2
+    "
+  >
+    <div class="col-span-1">
+      <div id="header" class="flex items-center mb-4">
         <img
           alt="avatar"
           class="w-20 rounded-full border-2 border-gray-300"
           src="https://picsum.photos/seed/picsum/200"
         />
-        <div class="ml-7">
-                        <p class="text-base leading-6 font-medium text-gray-700">
-                            WinneRose
-                        </p>
-                        <p class="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 ">
-                            @WinneRose
-                        </p>
-                        <i class="text-sm leading-5 font-small text-gray-400 group-hover:text-gray-300  font-open-sans">
-                            ~GTHRTHtr6htrHTRHrt6HTRh
-                        </i>
-            </div>
 
-             
+        <div class="ml-7">
+          <p class="text-base leading-6 font-medium text-gray-700">WinneRose</p>
+          <p
+            class="
+              text-sm
+              leading-5
+              font-medium
+              text-gray-400
+              group-hover:text-gray-300
+            "
+          >
+            @WinneRose
+          </p>
+          <i
+            class="
+              text-sm
+              leading-5
+              font-small
+              text-gray-400
+              group-hover:text-gray-300
+              font-open-sans
+            "
+          >
+            ~GTHRTHtr6htrHTRHrt6HTRh
+          </i>
+        </div>
       </div>
-      <div class="ml-24">
-<p class="text-base leading-6 font-medium text-gray-700">
-                            Followers
-                        </p>
-                        <p class="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 text-center">
-                            10
-                        </p>
+    </div>
+
+    <div class="col-span-1">
+      <div class="grid grid-cols-3 md:ml-26">
+        <div>
+          <p class="text-center leading-6 font-medium text-gray-700">
+            Followers
+          </p>
+          <p
+            class="
+              text-sm
+              leading-5
+              font-medium
+              text-gray-400
+              group-hover:text-gray-300
+              text-center
+            "
+          >
+            10
+          </p>
+        </div>
+        <div class="">
+          <p class="text-center leading-6 font-medium text-gray-700">
+            Following
+          </p>
+          <p
+            class="
+              text-sm
+              leading-5
+              font-medium
+              text-gray-400
+              group-hover:text-gray-300
+              text-center
+            "
+          >
+            10
+          </p>
+        </div>
+        <div class="">
+          <p class="text-center leading-6 font-medium text-gray-700">Posts</p>
+          <p
+            class="
+              text-sm
+              leading-5
+              font-medium
+              text-gray-400
+              group-hover:text-gray-300
+              text-center
+            "
+          >
+            10
+          </p>
+        </div>
       </div>
-       <div class="ml-9">
-<p class="text-base leading-6 font-medium text-gray-700">
-                            Following
-                        </p>
-                        <p class="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 text-center">
-                            10
-                        </p>
-      </div>
-       <div class="ml-9">
-<p class="text-base leading-6 font-medium text-gray-700">
-                            Posts
-                        </p>
-                        <p class="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 text-center">
-                            10
-                        </p>
-      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.rounded-full {
+  border-radius: 100%;
+}
 </style>
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,101 +1,98 @@
 <template>
-   <div class="bg-main">
+  <div class="bg-main">
     <HomePageTopbar></HomePageTopbar>
-    <profilecard class="mt-3"></profilecard>
-     <div class="mt-3 hidden md:block flex flex-col pt-2">
-   
-  </div>
     <div class="max-w-screen-xl mx-auto">
-      <div class="grid grid-cols-2 md:grid-cols-3 content-center">
-        <div class="col-span-2">
+      <div class="grid grid-cols-1 md:grid-cols-3 content-center">
+        <div class="col-span-1 md:col-span-2">
+          <profilecard class="mt-5"></profilecard>
+
           <Post v-for="post of posts" :post="post" :key="post.id" />
         </div>
-         <div class="h-screen mt-5">
-      <HomePageSidebarSection title="Top Communites">
-        <nav class="mt-4">
-          <a
-            class="
-              flex
-              items-center
-              mt-2
-              py-2
-              text-gray-600
-              border-white
-              hover:bg-gray-200
-              hover:text-gray-700
-              hover:border-gray-700
-              px-6
-            "
-            href="#"
-          >
-           <cryptoicon symbol="neo" size="24" color="black"/> 
-            <span class="mx-4 font-medium">NEO</span>
-          </a>
-          <a
-            class="
-              flex
-              items-center
-              mt-2
-              py-2
-              text-gray-600
-              border-white
-              hover:bg-gray-200
-              hover:text-gray-700
-              hover:border-gray-700
-              px-6
-            "
-            href="#"
-          >
-          <cryptoicon symbol="btc" size="24" color="black"/> 
-            <span class="mx-4 font-medium">BTC</span>
-          </a>
-          <a
-            class="
-              flex
-              items-center
-              mt-2
-              py-2
-              text-gray-600
-              border-white
-              hover:bg-gray-200
-              hover:text-gray-700
-              hover:border-gray-700
-              px-6
-            "
-            href="#"
-          >
-            <cryptoicon symbol="doge" size="24" color="black"/> 
-            <span class="mx-4 font-medium">DOGE</span>
-          </a>
-        </nav>
-      </HomePageSidebarSection>
+        <div class="h-screen mt-5 col-span-1">
+          <HomePageSidebarSection title="Top Communites">
+            <nav class="mt-4">
+              <a
+                class="
+                  flex
+                  items-center
+                  mt-2
+                  py-2
+                  text-gray-600
+                  border-white
+                  hover:bg-gray-200
+                  hover:text-gray-700
+                  hover:border-gray-700
+                  px-6
+                "
+                href="#"
+              >
+                <cryptoicon symbol="neo" size="24" color="black" />
+                <span class="mx-4 font-medium">NEO</span>
+              </a>
+              <a
+                class="
+                  flex
+                  items-center
+                  mt-2
+                  py-2
+                  text-gray-600
+                  border-white
+                  hover:bg-gray-200
+                  hover:text-gray-700
+                  hover:border-gray-700
+                  px-6
+                "
+                href="#"
+              >
+                <cryptoicon symbol="btc" size="24" color="black" />
+                <span class="mx-4 font-medium">BTC</span>
+              </a>
+              <a
+                class="
+                  flex
+                  items-center
+                  mt-2
+                  py-2
+                  text-gray-600
+                  border-white
+                  hover:bg-gray-200
+                  hover:text-gray-700
+                  hover:border-gray-700
+                  px-6
+                "
+                href="#"
+              >
+                <cryptoicon symbol="doge" size="24" color="black" />
+                <span class="mx-4 font-medium">DOGE</span>
+              </a>
+            </nav>
+          </HomePageSidebarSection>
 
-      <HomePageSidebarSection title="Top Tags">
-        <nav class="mt-4">
-          <div class="mt-2 px-6">
-            <span class="mx-2 text-tags">#Bitcoin</span><br />
-            <span class="mx-2 font-medium">3000 Tags</span>
-            <hr class="mt-3" width="200px" />
-          </div>
-          <div class="mt-2 px-6">
-            <span class="mx-2 font-medium">#Bitcoin</span><br />
-            <span class="mx-2 font-medium">3000 Tags</span>
-            <hr class="mt-3" width="200px" />
-          </div>
-          <div class="mt-2 px-6">
-            <span class="mx-2 font-medium">#Bitcoin</span><br />
-            <span class="mx-2 font-medium">3000 Tags</span>
-            <hr class="mt-3" width="200px" />
-          </div>
-          <div class="mt-2 px-6">
-            <span class="mx-2 font-medium">#Bitcoin</span><br />
-            <span class="mx-2 font-medium">3000 Tags</span>
-            <hr class="mt-3" width="200px" />
-          </div>
-        </nav>
-      </HomePageSidebarSection>
-      
-    </div>
+          <HomePageSidebarSection title="Top Tags">
+            <nav class="mt-4">
+              <div class="mt-2 px-6">
+                <span class="mx-2 text-tags">#Bitcoin</span><br />
+                <span class="mx-2 font-medium">3000 Tags</span>
+                <hr class="mt-3" width="200px" />
+              </div>
+              <div class="mt-2 px-6">
+                <span class="mx-2 font-medium">#Bitcoin</span><br />
+                <span class="mx-2 font-medium">3000 Tags</span>
+                <hr class="mt-3" width="200px" />
+              </div>
+              <div class="mt-2 px-6">
+                <span class="mx-2 font-medium">#Bitcoin</span><br />
+                <span class="mx-2 font-medium">3000 Tags</span>
+                <hr class="mt-3" width="200px" />
+              </div>
+              <div class="mt-2 px-6">
+                <span class="mx-2 font-medium">#Bitcoin</span><br />
+                <span class="mx-2 font-medium">3000 Tags</span>
+                <hr class="mt-3" width="200px" />
+              </div>
+            </nav>
+          </HomePageSidebarSection>
+        </div>
       </div>
     </div>
   </div>
@@ -120,20 +117,18 @@ const dummyData = [
     shareCount: 0,
     date: Date.now(),
   },
-  
-  
 ];
 
 import rsidebar from "../components/HomePage/sidebar/rsidebar.vue";
 import Post from "../components/post.vue";
-import Profilecard from '../components/ProfilePage/profilecard.vue';
+import Profilecard from "../components/ProfilePage/profilecard.vue";
 export default {
-     data() {
+  data() {
     return {
       posts: dummyData,
     };
   },
-  components: { rsidebar,Post, Profilecard }
+  components: { rsidebar, Post, Profilecard },
 };
 </script>
 
