98afb4eba8776444b7cd361f6d9b5452ba5f2225
kadircekim07 <33603691+kadircekim07@users.noreply.github.com>
2021-07-06 16:06:59 +0300

    nuxt link instead of href

--- components/libertabutton.vue
+++ components/libertabutton.vue
@@ -1,6 +1,7 @@
 <template>
-  <a
-    v-bind:href="click"
+  <nuxt-link
+    :to="click"
+    v-if="click != undefined"
     class="
       block
       mt-4
@@ -15,13 +16,32 @@
       cursor-pointer
     "
   >
-   {{text}}
+    {{ text }}
+  </nuxt-link>
+  <a
+    v-bind:href="click"
+    v-else
+    class="
+      block
+      mt-4
+      lg:inline-block
+      lg:mt-0
+      px-4
+      py-2
+      rounded
+      hover:bg-searchbarBG
+      mr-2
+      text-libertia
+      cursor-pointer
+    "
+  >
+    {{ text }}
   </a>
 </template>
 
 <script>
 export default {
-  props: ['text', 'click'],
+  props: ["text", "click"],
 };
 </script>
 
