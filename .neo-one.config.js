febfe66f92995ac6668ad6fd3b046416883e6ca1
kadircekim07 <33603691+kadircekim07@users.noreply.github.com>
2021-07-07 19:38:22 +0300

    lib

--- .neo-one.config.js
+++ .neo-one.config.js
@@ -8,8 +8,6 @@ module.exports = {
     path: 'neo-one/contracts',
     // Set this to true if you want the compile command to output JSON.
     json: true,
-    // Set this to true if you want the compile command to output AVM.
-    avm: false,
     // Set this to true if you want the compile command to output additional debug information.
     debug: false,
     // Set this to true if you want the compile command to output the AVM in a human-readable format for debugging (requires debug: true).
